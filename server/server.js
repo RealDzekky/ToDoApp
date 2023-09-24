69d921f42bc255605c9f04af2f3f3da6a72f2c41
Alexander Zhukov <dzekovalex@gmail.com>
2023-09-24 19:15:08 +0300

    Show todos only to specific email

--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@ const app =  express();
 const pool = require('./db');
 
 app.get('/todos', async (req, res) => {
+    const userEmail = 'dzekovalex@gmail.com';
     try{
-        const todos = await pool.query("SELECT * FROM todos")
+        const todos = await pool.query("SELECT * FROM todos WHERE user_email = $1", [userEmail])
         res.json(todos.rows)
     }catch(err){
         console.error(err);
