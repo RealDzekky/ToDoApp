6182567bfe50cc8649adf57297acaec536ead26f
Alexander Zhukov <dzekovalex@gmail.com>
2023-09-24 17:20:09 +0300

    Fixed .env to succesfully connect to database

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app =  express();
 const pool = require('./db');
 
 app.get('/todos', async (req, res) => {
     try{
-        const todos = await pool.query('SELECT * FROM todos');
-        res.json(todos.rows);
+        const todos = await pool.query("SELECT * FROM todos")
+        res.json(todos.rows)
     }catch(err){
         console.error(err);
     }
