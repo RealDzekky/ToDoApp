510c168f4ca3292f2e29699d265770e25e189fc5
Alexander Zhukov <dzekovalex@gmail.com>
2023-09-25 22:11:28 +0300

    Show only loged user todos

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 const express = require('express');
 const cors = require('cors');
 const app =  express();
 const pool = require('./db');
 app.use(cors());
 
-app.get('/todos/', async (req, res) => {
-    const userEmail = req.params;
+app.get('/todos/:userEmail', async (req, res) => {
+    const { userEmail } = req.params;
     try{
-        const todos = await pool.query("SELECT * FROM todos WHERE user_email = $1", [userEmail])
+        const todos = await pool.query("SELECT * FROM todos WHERE user_email = $1", [userEmail]);
         res.json(todos.rows)
     }catch(err){
         console.error(err);
