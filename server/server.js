c763eee1fe6681ac816072a36cc68156fd07ce60
Alexander Zhukov <dzekovalex@gmail.com>
2023-10-03 23:48:02 +0300

    Now you can edit your todos!

--- server/server.js
+++ server/server.js
@@ -3,8 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const app =  express();
 const pool = require('./db');
+const {v4: uuidv4} = require('uuid');
 app.use(cors());
+app.use(express.json());
 
+// get all todos
 app.get('/todos/:userEmail', async (req, res) => {
     const { userEmail } = req.params;
     try{
@@ -14,6 +17,35 @@ app.get('/todos/:userEmail', async (req, res) => {
         console.error(err);
     }
 });
+
+// create new todo
+
+app.post('/todos', async (req, res) => {
+    const { user_email, title, progress, date } = req.body;
+    const id = uuidv4();
+    console.log(user_email, title, progress, date);
+    try {
+        const newToDo = await pool.query('INSERT INTO todos(id, user_email, title, progress, date) VALUES($1, $2, $3, $4, $5)',
+            [id, user_email, title, progress, date]);
+        res.json(newToDo);
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+//edit todo
+
+app.put('/todos/:id', async (req, res) => {
+    const {id} = req.params;
+    const { user_email, title, progress, date } = req.body;
+    try{
+        const editToDo = await pool.query('UPDATE todos SET user_email = $1, title = $2, progress = $3, date = $4 WHERE id = $5;', 
+        [user_email, title, progress, date, id]);
+        res.json(editToDo);
+    }catch(err){
+        console.log(err);
+    }
+})
 
 app.listen(PORT, () => console.log(`SERVER RUNNING ON PORT ${PORT}`));
 
