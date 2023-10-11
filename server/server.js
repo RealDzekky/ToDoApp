232e5e8fb21ed28f1050bed2426eeadc9b8d8024
Alexander Zhukov <dzekovalex@gmail.com>
2023-10-11 13:48:03 +0300

    Finnal commit

--- server/server.js
+++ server/server.js
@@ -66,19 +66,26 @@ app.post('/signup', async (req, res) => {
     const { email, password } = req.body;
     const salt = bcrypt.genSaltSync(10);
     const hashedPassword = bcrypt.hashSync(password, salt);
-    try{
-        const signUp = await pool.query('INSERT INTO users (email, hashed_password) VALUES($1, $2)',
-            [email, hashedPassword]);
 
-        const token = jwt.sign({ email }, 'secret', { expiresIn: '1hr'});
-        res.json({ email, token});
-    } catch(err){
+    try {
+        // Check if the user already exists
+        const userExists = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        if (userExists.rows.length > 0) {
+            return res.json({ detail: 'User with this email already exists' });
+        }
+
+        // If the user doesn't exist, insert the new user
+        const signUp = await pool.query('INSERT INTO users (email, hashed_password) VALUES($1, $2)', [email, hashedPassword]);
+        const token = jwt.sign({ email }, 'secret', { expiresIn: '1hr' });
+        res.json({ email, token });
+    } catch (err) {
         console.error(err);
-        if(err){
-            res.json({ detail : err.detail })
+        if (err) {
+            res.json({ detail: err.detail });
         }
     }
-})
+});
+
 
 //login
 app.post('/login', async (req, res) => {
